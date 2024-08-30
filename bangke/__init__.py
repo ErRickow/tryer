048c44a5eaa9e9b56b3422152894d34196972c77
Just R <remixingmagelang@gmail.com>
2024-08-31 01:52:56 +0700

    update bangke/__init__.py

--- bangke/__init__.py
+++ bangke/__init__.py
@@ -11,6 +11,69 @@ class Config:
     pass
 
 
-def isLocalHost():
+  def isLocalHost():
     """Check if it is localhost"""
     return os.path.exists("config.txt")
+
+    def setup_config(self):
+        count = 1
+        #self.clear_screen
+
+        # check requirements & install
+        #self.check_requirements()
+        #self.clear_screen
+
+        # check if the user config file exists
+        if os.path.exists("config.txt"):
+            print("config.txt file exists: Yes\n\n")
+            with open("config.txt") as f:
+                content = [x for x in f.read().split("\n") if x not in ("\n", "")]
+
+            # set text file config values
+            print(Colors.block + "Setting configuration values.\n\n" + Colors.reset)
+            for x in content:
+                data = x.split("=")
+                file_value = data[1]
+                if data[1].isdigit():
+                    file_value = int(data[1])
+
+                setattr(Config, data[0], file_value)
+                print(f"[{count}] Added config = {data[0]} with value = {file_value}\n")
+                count += 1
+
+        else:
+            print("config.txt file doesn't exist, existing. . .")
+            exit(0)
+
+        # set remaining necessary config values
+        print(Colors.block + "\nSetting remaining configuration values\n\n" + Colors.reset)
+        for attr in dir(Configuration):
+            value = getattr(Configuration, attr, None)
+
+            if attr.isupper() and not hasattr(Config, attr):
+                setattr(Config, attr, value)
+                print(f"[{count}] Added config = {attr} with value = {value}\n")
+                count += 1
+
+        clear = input(f"{Colors.block}Should I clear the screen ?{Colors.reset} (Y/N): ")
+        if (not clear) or (clear and clear.upper() == "Y"):
+            self.clear_screen
+
+
+
+tools = Tools()
+
+if(isLocalHost()):
+    tools.setup_config()
+else:
+    print("Setting the Non-LocalHost setup ... !")
+    for attr in dir(Configuration):
+        value = getattr(Configuration, attr, None)
+        if attr.isupper() and not hasattr(Config, attr):
+            setattr(Config, attr, value)
+
+
+# default import
+from main.userbot import app
+bot = app.bot
+from main.core.filters import gen
