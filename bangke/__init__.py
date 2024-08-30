403f73c1f7424fcd2eeba42433a73cdff7e6ca71
Just R <remixingmagelang@gmail.com>
2024-08-31 03:13:08 +0700

    update bangke/__init__.py

--- bangke/__init__.py
+++ bangke/__init__.py
@@ -17,11 +17,11 @@ class Config:
 
     def setup_config(self):
         count = 1
-        #self.clear_screen
+        self.clear_screen
 
         # check requirements & install
-        #self.check_requirements()
-        #self.clear_screen
+        self.check_requirements()
+        self.clear_screen
 
         # check if the user config file exists
         if os.path.exists("config.txt"):
