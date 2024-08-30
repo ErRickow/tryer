7eceda00a7ed6c7c38bc661945ac68f4e0d63dee
Just R <remixingmagelang@gmail.com>
2024-08-31 01:50:50 +0700

    update bangke/__init__.py

--- bangke/__init__.py
+++ bangke/__init__.py
@@ -2,6 +2,15 @@ import os
 import socket
 import platform
 import subprocess
-from importlib import metadata
+#from importlib import metadata
 from bangke.others import Colors
 from config import Configuration
+
+class Config:
+    """This class generates the configs """
+    pass
+
+
+def isLocalHost():
+    """Check if it is localhost"""
+    return os.path.exists("config.txt")
