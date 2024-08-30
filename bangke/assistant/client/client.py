7fc3749d468d2fada3a54a80afd8b49f15afe4e7
Just R <remixingmagelang@gmail.com>
2024-08-31 04:32:56 +0700

    update bangke/assistant/client/client.py

--- bangke/assistant/client/client.py
+++ bangke/assistant/client/client.py
@@ -18,36 +18,36 @@ from bangke.core import Core
 class Bot(Core, Client):
     """ Assistant (Nora) """
     def __init__(self):
-      super().__init__(
-        name="Sugeng",
-        api_id=self.API_ID,
-        api_hash=self.API_HASH,
-        bot_token=self.BOT_TOKEN
+        super().__init__(
+            name="Sugeng",
+            api_id=self.API_ID,
+            api_hash=self.API_HASH,
+            bot_token=self.BOT_TOKEN
     )
-    try:
-      self.start()
-      self.me = self.get_chat("me")
-      self.id = self.me.id
-      self.dc_id = self.me.dc_id
-      self.name = self.me.first_name
-      self.username = f"@{self.me.username}"
-      self.bio = self.me.bio if self.me.bio else ""
-      dp_name = f"dp_{self.id}.jpg"
-      dp_path = f"./downloads/{dp_name}"
-      if not os.path.exists(dp_path):
-        self.pic = self.download_media(self.me.photo.big_file_id, dp_name) if self.me.photo else None
-        self.is_bot = True
-        self.stop()
-      #  except FloodWait as e:
-       #     print(e)
+        try:
+            self.start()
+            self.me = self.get_chat("me")
+            self.id = self.me.id
+            self.dc_id = self.me.dc_id
+            self.name = self.me.first_name
+            self.username = f"@{self.me.username}"
+            self.bio = self.me.bio if self.me.bio else ""
+            dp_name = f"dp_{self.id}.jpg"
+            dp_path = f"./downloads/{dp_name}"
+            if not os.path.exists(dp_path):
+                self.pic = self.download_media(self.me.photo.big_file_id, dp_name) if self.me.photo else None
+            self.is_bot = True
+            self.stop()
+        except FloodWait as e:
+            print(e)
 
         self.__class__.__module__ = "pyrogram.client"
 
 
-        async def start_assistant(self):
-       # """ this function starts the pyrogram bot client. """
+    async def start_assistant(self):
+        """ this function starts the pyrogram bot client. """
 
-          try:
+        try:
             if not self:
                 raise Exception("The userbot client is missing.")
                 quit(0)
@@ -80,3 +80,5 @@ class Bot(Core, Client):
                     print("Added bot commands.\n")
             else:
                 print("Assistant is not activated.\n")
+        except Exception:
+            print(traceback.format_exc())
