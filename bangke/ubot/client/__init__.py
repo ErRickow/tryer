e1cda54985eab1d77da43b6808a57525df1a9aa6
Just R <remixingmagelang@gmail.com>
2024-08-31 04:21:31 +0700

    update bangke/ubot/client/__init__.py

--- bangke/ubot/client/__init__.py
+++ bangke/ubot/client/__init__.py
@@ -8,7 +8,7 @@ try:
 except ImportError:
     print(f"{Colors.red}\nuvloop wasn't installed, userbot will work slow.{Colors.reset}\n")
 
-from bangke.ubot.client import SuperClient
+from bangke.ubot.client.client import SuperClient
 
 
 app = SuperClient()
