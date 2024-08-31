303adf1fa10ea4b647ac9398102cc472fb690e85
Just R <remixingmagelang@gmail.com>
2024-08-31 18:22:36 +0700

    update bangke/ubot/modules/plugins/sat.py

---
+++ bangke/ubot/modules/plugins/sat.py
@@ -0,0 +1,8 @@
+import asyncio
+import random
+import time
+from datetime import datetime as dt
+
+from pyrogram import Client, filters
+from pyrogram.types import *
+from pyrogram.types import Message
