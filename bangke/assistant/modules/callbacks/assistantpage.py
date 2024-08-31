88c2b7bfe22ec097b5cc9a1a1dfe3fd91f1395f1
Just R <remixingmagelang@gmail.com>
2024-08-31 15:51:27 +0700

    update bangke/assistant/modules/callbacks/assistantpage.py

---
+++ bangke/assistant/modules/callbacks/assistantpage.py
@@ -0,0 +1,30 @@
+from pyrogram import filters
+from pyrogram.types import (
+    InputMediaPhoto,
+    CallbackQuery
+)
+
+from bangke.ubot.client import app
+
+
+
+
+
+@app.bot.on_callback_query(filters.regex("assistant-tab"))
+@app.alert_user
+async def _assistant(_, cb: CallbackQuery):
+    try:
+        await cb.edit_message_media(
+            media=InputMediaPhoto(
+                media="main/others/resources/images/nora.png",
+                caption=app.assistant_tab_string
+            ),
+            reply_markup=app.buildMarkup(
+                [
+                    app.buildButton("Home", "close-tab"),
+                    app.buildButton("Back", "home-tab")
+                ]
+            )
+        )
+    except Exception as e:
+        await app.error(e)
