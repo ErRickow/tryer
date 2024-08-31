5a7d8fdb69b7872233210ee218b7488cd2f65e23
Just R <remixingmagelang@gmail.com>
2024-08-31 17:04:39 +0700

    update bangke/assistant/modules/callbacks/closepage.py

---
+++ bangke/assistant/modules/callbacks/closepage.py
@@ -0,0 +1,29 @@
+from pyrogram import filters
+
+from pyrogram.types import (
+    InputMediaPhoto,
+    CallbackQuery
+)
+
+from bangke.ubot.client import app
+
+
+
+
+
+@app.bot.on_callback_query(filters.regex("close-tab"))
+@app.alert_user
+async def close_helpmenu_callback(_, cb: CallbackQuery):
+    try:
+        await cb.edit_message_media(
+            media=InputMediaPhoto(
+                media="main/others/resources/images/tron-square.png", 
+                caption=app.close_tab_string
+            ),
+            reply_markup=app.buildMarkup(
+                [app.buildButton("Open", "home-tab")],
+                [app.buildButton("Delete", "delete-tab")]
+            )
+        )
+    except Exception as e:
+        await app.error(e)
