c41e1a24d1dce5986cc9670eba24040d563388cf
Just R <remixingmagelang@gmail.com>
2024-08-31 15:30:32 +0700

    update bangke/__init__.py

--- bangke/__init__.py
+++ bangke/__init__.py
@@ -81,7 +81,7 @@ class Tools:
 
         clear = input(f"{Colors.block}Should I clear the screen?{Colors.reset} (Y/N): ")
         if not clear or (clear.upper() == "Y"):
-            self.clear_screen()  # Call the method
+           # self.clear_screen()  # Call the method
 
 
 tools = Tools()
