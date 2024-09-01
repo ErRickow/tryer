6bd95d0d99ed429110c12660908c0baa213415b2
Just R <remixingmagelang@gmail.com>
2024-09-02 03:47:18 +0700

    update bangke/ubot/modules/plugins/bantuan.py

--- bangke/ubot/modules/plugins/bantuan.py
+++ bangke/ubot/modules/plugins/bantuan.py
@@ -2,44 +2,38 @@
 
 import os
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineQueryResultPhoto, InlineKeyboardMarkup, ParseMode
 from pyrogram.errors import BotInlineDisabled
 from bangke import app, gen
-
-async def get_help_inline(m: Message):
-    """ Mengambil hasil inline untuk menu bantuan """
-    try:
-        result = await app.get_inline_bot_results(app.bot.username, "#helpmenu")
-        if result:
-            await m.delete()
-            await app.send_inline_bot_result(
-                m.chat.id,
-                query_id=result.query_id,
-                result_id=result.results[0].id,
-                disable_notification=True,
-            )
-        else:
-            await app.send_edit(
-                "Please check if your bot's inline mode is enabled . . .",
-                delme=3,
-                text_type=["mono"]
-            )
-    except Exception as e:
-        await app.error(e)
 
 @app.on_cmd(
     commands="help",
     usage="Get your helpmenu, use plugin name as suffix to get command information.",
 )
 async def helpmenu_handler(_, m: Message):
-    """ Handler untuk menu bantuan """
+    """ helpmenu handler for help plugin """
+
     args = m.command or m.sudo_message.command or []
-    args_exists = len(args) > 1
 
     try:
-        if not args_exists:
-            await app.send_edit(". . .", text_type=["mono"])
-            await get_help_inline(m)
+        if len(args) <= 1:
+            await app.send_edit(". . .")
+
+            # Mengambil hasil inline query
+            result = await app.get_inline_bot_results(app.bot.username, "#helpmenu")
+            if result:
+                await m.delete()
+                await app.send_inline_bot_result(
+                    m.chat.id,
+                    query_id=result.query_id,
+                    result_id=result.results[0].id,
+                    disable_notification=True,
+                )
+            else:
+                await app.send_edit(
+                    "Please check if your bot's inline mode is on . . .",
+                    delme=3
+                )
         else:
             module_help = await app.PluginData(args[1])
             if not module_help:
@@ -55,6 +49,43 @@ async def helpmenu_handler(_, m: Message):
     except Exception as e:
         await app.error(e)
 
+
+@app.bot.on_inline_query(filters.user(app.AllUsersId))
+async def inline_result(_, inline_query):
+    print(inline_query)
+    query = inline_query.query
+    emoji = app.HelpEmoji or "•"
+
+    if query.startswith("#helpmenu"):
+        await inline_query.answer(
+            results=[
+                InlineQueryResultPhoto(
+                    photo_url=app.BotPic,
+                    title="Tron Inline helpdex menu",
+                    description="Get your inline helpdex menu.",
+                    caption=app.home_tab_string,
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            app.BuildKeyboard(
+                                (
+                                    [f"{emoji} Settings {emoji}", "settings-tab"],
+                                    [f"{emoji} Plugins {emoji}", "plugins-tab"]
+                                )
+                            ),
+                            app.BuildKeyboard(
+                                (
+                                    [f"{emoji} Extra {emoji}", "extra-tab"],
+                                    [f"{emoji} Stats {emoji}", "stats-tab"]
+                                )
+                            ),
+                            app.BuildKeyboard(([["Assistant", "assistant-tab"]])),
+                            app.BuildKeyboard(([["Close", "close-tab"]]))
+]
+)
+)
+],
+cache_time=1
+)
 
 # get all module name
 @app.on_cmd(
