75aeacb0ee1476ca00ac10740737929f80ab3213
Just R <remixingmagelang@gmail.com>
2024-09-02 02:57:10 +0700

    update bangke/ubot/modules/plugins/cohere.py

---
+++ bangke/ubot/modules/plugins/cohere.py
@@ -0,0 +1,41 @@
+import asyncio
+import os
+from json import tool
+
+import cohere
+from pyrogram import Client, enums, filters
+from pyrogram.types import Message
+
+API_KEY=hPZY8Tf8TXUZRK3jzuOZyz0atWR9q7IzywrK4hTQ
+
+@Akeno(filters.command("cohere", CMD_HANDLER) & filters.me)
+async def coheres_(c: Client, message: Message):
+    co = cohere.Client(api_key=API_KEY)
+    try:
+        if len(message.command) > 1:
+            prompt = message.text.split(maxsplit=1)[1]
+        elif message.reply_to_message:
+            prompt = message.reply_to_message.text
+        else:
+            await message.reply_text(
+                "<b>Usage: </b><code>.cohere [prompt/reply to message]</code>"
+            )
+            return
+        response = co.chat(
+            prompt=prompt,
+            model="command-r-plus"
+        )
+        output = response.text
+        if len(output) > 4096:
+            with open("chat.txt", "w+", encoding="utf8") as out_file:
+                out_file.write(output)
+            await message.reply_document(
+                document="chat.txt",
+                disable_notification=True
+            )
+            os.remove("chat.txt")
+        else:
+            await message.reply_text(output, disable_web_page_preview=True)
+    except Exception as e:
+        await message.reply_text(f"An error occurred: {e}")
+```
