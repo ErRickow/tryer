95e8f510651987e050522c44b2614bf9d3ced331
Just R <remixingmagelang@gmail.com>
2024-09-02 02:46:56 +0700

    update bangke/ubot/modules/plugins/ai.py

--- bangke/ubot/modules/plugins/ai.py
+++ bangke/ubot/modules/plugins/ai.py
@@ -8,6 +8,14 @@ from bangke import app
 from bangke.core import filters
 from bangke.core.enums import HandlerType
 
+async def mistraai(messagestr):
+    url = "https://randydev-ryuzaki-api.hf.space/api/v1/akeno/mistralai"
+    payload = {"args": messagestr}
+    response = requests.post(url, json=payload)
+    if response.status_code != 200:
+        return None
+    return response.json()
+
 async def chatgptold(messagestr):
     url = "https://randydev-ryuzaki-api.hf.space/ryuzaki/chatgpt-old"
     payload = {"query": messagestr}
@@ -15,6 +23,39 @@ async def chatgptold(messagestr):
     if response.status_code != 200:
         return None
     return response.json()
+
+@app.on_update(
+    handler_type=HandlerType.MESSAGE,
+    filters=filters.gen(
+        commands="mistralai",
+        usage="tanya dengan ai."
+    )
+)
+async def mistralai_(client: Client, message: Message):
+    if len(message.command) > 1:
+        prompt = message.text.split(maxsplit=1)[1]
+    elif message.reply_to_message:
+        prompt = message.reply_to_message.text
+    else:
+        return await message.reply_text("Give ask from mistralai")
+    try:
+        messager = await mistraai(prompt)
+        if messager is None:
+            return await message.reply_text("No response")
+        output = messager["randydev"].get("message")
+        if len(output) > 4096:
+            with open("chat.txt", "w+", encoding="utf8") as out_file:
+                out_file.write(output)
+            await message.reply_document(
+                document="chat.txt",
+                disable_notification=True
+            )
+            os.remove("chat.txt")
+        else:
+            await message.reply_text(output)
+    except Exception as e:
+        LOGS.error(str(e))
+        return await message.reply_text(str(e))
 
 @app.on_update(
     handler_type=HandlerType.MESSAGE,
