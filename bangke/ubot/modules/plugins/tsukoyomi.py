979a24382f95db1cee9964320b4757d55153f7f0
Just R <remixingmagelang@gmail.com>
2024-09-02 23:47:32 +0700

    update bangke/ubot/modules/plugins/tsukoyomi.py

--- bangke/ubot/modules/plugins/tsukoyomi.py
+++ bangke/ubot/modules/plugins/tsukoyomi.py
@@ -23,19 +23,19 @@ async def zombies_handler(client, message):
     count = 0
 
     if app.long() != 2:
-        a = await message.reply("Sedang mngendus . . .")
+        a = await message.reply("Sedang mngendus . . .", quote=True)
 
         async for x in app.get_chat_members(chat_id=message.chat.id):
             if x.user.is_deleted:
                 temp_count += 1
 
         if temp_count > 0:
-            await a.edit_text(f"**Terdeteksi** `{temp_count}` Musuhnya Bapak Naruto\nGunakan `{app.Trigger[0]}tsukoyomi untuk melenyapkannya dari bumi.")
+            await a.edit_text(f"Terdeteksi {temp_count} Musuhnya Bapak Naruto\nGunakan {app.Trigger[0]}tsukoyomi untuk melenyapkannya dari bumi.", quote=True)
         else:
-            await a.edit_text("KGA ADA MUSUH YANG TERDETEKSI EMHH.\nGroup bersih nan sentosa ! 😃")
+            await a.edit_text("KGA ADA MUSUH YANG TERDETEKSI EMHH.\nGroup bersih nan sentosa ! 😃", quote=True)
 
-    elif app.long() == 2 and a.command[1] == "kyaa":
-        await a.edit_text("**SHINRA TENSEI** . . .")
+    elif app.long() == 2 and message.command[1] == "kyaa":
+        await a.edit_text("*SHINRA TENSEI* . . .", quote=True)
 
         async for x in app.get_chat_members(chat_id=message.chat.id):
             if x.user.is_deleted:
@@ -51,9 +51,9 @@ async def zombies_handler(client, message):
                     await asyncio.sleep(0.2)
                 except Exception as e:
                     await app.error(e)
-        await a.edit_text(f"`WATASHIWA DONE`\n\n**Total:** `{count+admin_count}`\n**Removed:** `{count}`\n**Removed:** `{admin_count}`\n\n**Note:** `Not removed accounts can be admins or the owner`")
+        await a.edit_text(f"WATASHIWA DONE\n\n*Total:* {count+admin_count}\n*Removed:* {count}\n*Not Removed:* {admin_count}\n\n*Note:* Not removed accounts can be admins or the owner", quote=True)
 
-    elif app.long() == 2 and a.command[1] != "kyaa":
-        await a.edit_text(f"Check `{app.Trigger[0]}help tsukoyomi` to see how it works !")
+    elif app.long() == 2 and message.command[1] != "kyaa":
+        await a.edit_text(f"Check {app.Trigger[0]}help tsukoyomi` to see how it works !", quote=True)
     else:
-        await a.edit_text("Something went wrong, please try again later !")
+        await a.edit_text("Something went wrong, please try again later !", quote=True)
