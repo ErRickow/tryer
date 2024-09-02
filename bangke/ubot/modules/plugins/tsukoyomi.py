a6c236925712978ccf665057ecde715ab62d71ba
Just R <remixingmagelang@gmail.com>
2024-09-02 23:53:35 +0700

    update bangke/ubot/modules/plugins/tsukoyomi.py

--- bangke/ubot/modules/plugins/tsukoyomi.py
+++ bangke/ubot/modules/plugins/tsukoyomi.py
@@ -30,12 +30,12 @@ async def zombies_handler(client, message):
                 temp_count += 1
 
         if temp_count > 0:
-            await a.edit_text(f"Terdeteksi {temp_count} Musuhnya Bapak Naruto\nGunakan {app.Trigger[0]}tsukoyomi untuk melenyapkannya dari bumi.", quote=True)
+            await message.reply(f"Terdeteksi {temp_count} Musuhnya Bapak Naruto\nGunakan {app.Trigger[0]}tsukoyomi untuk melenyapkannya dari bumi.", quote=True)
         else:
-            await a.edit_text("KGA ADA MUSUH YANG TERDETEKSI EMHH.\nGroup bersih nan sentosa ! 😃", quote=True)
+            await message.reply("KGA ADA MUSUH YANG TERDETEKSI EMHH.\nGroup bersih nan sentosa ! 😃", quote=True)
 
     elif app.long() == 2 and message.command[1] == "kyaa":
-        await a.edit_text("*SHINRA TENSEI* . . .", quote=True)
+        await message.reply("*SHINRA TENSEI* . . .", quote=True)
 
         async for x in app.get_chat_members(chat_id=message.chat.id):
             if x.user.is_deleted:
@@ -51,9 +51,9 @@ async def zombies_handler(client, message):
                     await asyncio.sleep(0.2)
                 except Exception as e:
                     await app.error(e)
-        await a.edit_text(f"WATASHIWA DONE\n\n*Total:* {count+admin_count}\n*Removed:* {count}\n*Not Removed:* {admin_count}\n\n*Note:* Not removed accounts can be admins or the owner", quote=True)
+        await message.reply(f"WATASHIWA DONE\n\n*Total:* {count+admin_count}\n*Removed:* {count}\n*Not Removed:* {admin_count}\n\n*Note:* Not removed accounts can be admins or the owner", quote=True)
 
     elif app.long() == 2 and message.command[1] != "kyaa":
-        await a.edit_text(f"Check {app.Trigger[0]}help tsukoyomi` to see how it works !", quote=True)
+        await message.reply(f"Check {app.Trigger[0]}help tsukoyomi` to see how it works !", quote=True)
     else:
-        await a.edit_text("Something went wrong, please try again later !", quote=True)
+        await message.reply("Something went wrong, please try again later !", quote=True)
